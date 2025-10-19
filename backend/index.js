acafb5d0cb16b29fedd0a9c1866efc3216890c83
Tran Minh Tri <minhtri084038@gmail.com>
2025-10-19 23:46:40 +0700

    Init project

---
+++ backend/index.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const app = express();
+const PORT = 3000;
+
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+  res.send("Hello Node.js 🚀");
+});
+
+
+app.listen(PORT, () => {
+  console.log(`Server đang chạy tại http://localhost:${PORT}`);
+});
